de551812b69d0d13076ba7a189dcdb103c45eedf
markzampoglou <markzampoglou@gmail.com>
2016-02-11 13:15:57 +0200

    First commit

---
+++ ExportDCT.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "jpeglib.h"
+#include "jerror.h"
+
+int testInOut(int);
+int * getDCT(char *InputFileName);
---
+++ TestDCT.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "jpeglib.h"
+#include "jerror.h"
+#include "jconfig.h"
+#include "ExportDCT.h"
+
+int main( int argc, const char* argv[]){
+	int * a;
+	a= getDCT("./068.jpg");
+	
+	int i;
+	printf("%i \n",a[0]);
+	printf("%i \n",a[1]);
+	printf("%i \n",a[1602]);
+	return 0;
+}
